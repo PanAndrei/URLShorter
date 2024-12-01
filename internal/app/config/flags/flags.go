4bda48d4e4b233c0fac20e6fbaf1b079222d687b
Andrei Panasenko <andrei_kmv@inbox.ru>
2024-12-02 00:28:22 +0300

    config

--- internal/app/config/flags/flags.go
+++ internal/app/config/flags/flags.go
@@ -3,6 +3,7 @@ package flags
 import (
 	hadlCnfg "URLShorter/internal/app/handlers/config"
 	"flag"
+	"os"
 )
 
 type mainConfig struct {
@@ -16,5 +17,14 @@ var Cnfg = mainConfig{
 func ParsFlags() {
 	flag.StringVar(&Cnfg.Handlers.ServerAdress, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&Cnfg.Handlers.ReturnAdress, "b", "http://localhost:8080", "redirect adress")
+
+	if serverAdress := os.Getenv("SERVER_ADDRESS"); serverAdress != "" {
+		Cnfg.Handlers.ServerAdress = serverAdress
+	}
+
+	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+		Cnfg.Handlers.ReturnAdress = baseURL
+	}
+
 	flag.Parse()
 }
