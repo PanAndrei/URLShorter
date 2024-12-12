7eda2a2cda0b6924cdaa550be5c07f826d4b3635
Andrei Panasenko <andrei_kmv@inbox.ru>
2024-12-12 16:36:57 +0300

    добавил логгер

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,21 +3,24 @@ package main
 import (
 	flags "URLShorter/internal/app/config/flags"
 	handlers "URLShorter/internal/app/handlers"
+	log "URLShorter/internal/app/logger"
 	repo "URLShorter/internal/app/repository"
 	serv "URLShorter/internal/app/service"
-	"fmt"
-	"log"
 )
 
 func main() {
 	flags.ParsFlags()
-	fmt.Println("Running server on", flags.Cnfg.Handlers.ServerAdress)
+
 	if err := run(); err != nil {
-		log.Fatal(err)
+		// log fatal
 	}
 }
 
 func run() error {
+	if err := log.Initialize("info"); err != nil {
+		return err
+	}
+
 	repo := repo.NewStore()
 	shorter := serv.NewShorter(repo)
 
